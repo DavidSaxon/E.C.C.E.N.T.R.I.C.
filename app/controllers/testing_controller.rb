41b28563d83dff8d741ea8a7f7f67bed85326985
David Saxon <davidesaxon@gmail.com>
2013-08-19 10:26:46 +1200

    added example page

---
+++ app/controllers/testing_controller.rb
@@ -0,0 +1,4 @@
+class TestingController < ApplicationController
+  def example
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 ECCENTRIC::Application.routes.draw do
+  get "testing/example"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root to: 'testing#example'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
