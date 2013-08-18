37a05d673dffcda96ad7a6759c729f38bad69179
David Saxon <davidesaxon@gmail.com>
2013-08-19 10:02:35 +1200

    rails generated code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ECCENTRIC::Application.config.secret_key_base = '506d85f3ab379e4cbb40e6ac8fec77efe269d122541f6c4c50b6d38c7fe064adacb10ce6f3cf9ad200721668f0121073bff318cd6ef0a9c3eefe9cbe178f3f6c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ECCENTRIC::Application.config.session_store :cookie_store, key: '_E_C_C_E_N_T_R_I_C__session'
