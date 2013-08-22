f3d494e1bfed97307ef2c387361023eb81c78986
jono <hippo1081@hotmail.com>
2013-08-22 14:03:41 +1200

    positions appear on map

--- app/controllers/map_points_controller.rb
+++ app/controllers/map_points_controller.rb
@@ -7,7 +7,7 @@ class MapPointsController < ApplicationController
 	end
 
 	def update
-		@map_point = MapPoint.new(title: params[:title], description: params[:description], x: params[:x], y: params[:y]) #params[:put].permit(:type, :description, :x, :y)
+		@map_point = MapPoint.new(title: params[:title], description: params[:description], x: params[:x], y: params[:y])
   		@map_point.save
   		redirect_to root_path
 
